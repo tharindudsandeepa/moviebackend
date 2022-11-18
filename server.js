550e7b5a4feb1e232de53ff0e4fb6da463c6ad18
Tharindu Sandeepa <73959253+tharindudsandeepa@users.noreply.github.com>
2022-11-18 07:46:02 +0530

    first

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(session({
-    secret: process.env.SECRET,
+    secret: "somevalue",
     resave: false,
     saveUninitialized: false
 }));
